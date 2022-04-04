b758e5ea709701cf27f37ffe52a1cf03ee1fc095
Ras Ally <rasally434@gmail.com>
2022-04-04 17:44:02 -0400

    Add algos

---
+++ frontend/src/components/Algorithms/all_algos_container.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { connect } from "react-redux";
+import AllAlgos from "./all_algos";
+
+const mSTP = (state) => {
+
+  return {
+    
+  }
+}
+
+const mDTP = (dispatch) => ({
+
+})
+
+export default connect(mSTP, mDTP)(AllAlgos)
