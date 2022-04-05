24f23d8b1db8ab4580f4aaf527b171d2c5da6aa0
Ras Ally <rasally434@gmail.com>
2022-04-04 20:00:07 -0400

    Add seeds for table

--- frontend/src/components/Algorithms/all_algos.jsx
+++ frontend/src/components/Algorithms/all_algos.jsx
@@ -8,25 +8,56 @@ const AllAlgos = () => {
   const data = React.useMemo(
     () => [
       {
-        col1: 'Hello',
-        col2: 'World',
+        name: "Reverse Bits",
+        category: "Binary",
+        link: "https://leetcode.com/problems/reverse-bits/",
+        videoSolution: "https://youtu.be/UcoN6UjAI64",
+        textSolution: "reverse each of 32 bits",
+        difficulty: "Easy",
       },
       {
-        col1: 'react-table',
-        col2: 'rocks',
+        name: "Word Search II",
+        category: "Tree",
+        link: "https://leetcode.com/problems/word-search-ii/",
+        videoSolution: "https://youtu.be/asbcE9mZz_U",
+        textSolution:
+          "trick: I though use trie to store the grid, reverse thinking, instead store dictionary words, dfs on each cell, check if cell's char exists as child of root node in trie, if it does, update currNode, and check neighbors, a word could exist multiple times in grid, so don’t add duplicates",
+          difficulty: "Hard",
+        },
+        {
+        name: "Add and Search Word",
+        category: "Tree",
+        link: "https://leetcode.com/problems/add-and-search-word-data-structure-design/",
+        videoSolution: "https://youtu.be/BTf05gs_8iU",
+        textSolution:
+        'if char = "." run search for remaining portion of word on all of curr nodes children;',
+        difficulty: "Medium",
       },
       {
-        col1: 'whatever',
-        col2: 'you want',
+        name: 'Two Sum',
+        category: 'Arrays',
+        link: "https://leetcode.com/problems/two-sum/",
+        videoSolution: "https://youtu.be/KLlXCFG5TnA",
+        textSolution:
+          "Use hash map to instantly check for difference value, map will add index of last occurrence of a num, don't use same element twice",
+        difficulty: "Easy",
       },
       {
-        col1: 'only column 1'
+        name: "Minimum Window Substring",
+        category: "String",
+        link: "https://leetcode.com/problems/minimum-window-substring/",
+        videoSolution: "https://youtu.be/jSto0O4AJbM",
+        textSolution:
+          "need is num of unique char in T, HAVE is num of char we have valid count for, sliding window, move right until valid, if valid, increment left until invalid, to check validity keep track if the count of each unique char is satisfied",
+        difficulty: "Hard",
       },
       {
-        banana: 'bananable'
-      },
-      {
-        col2: 'only column 2'
+        name: "Maximum Product Subarray",
+        category: "Arrays",
+        link: "https://leetcode.com/problems/maximum-product-subarray/",
+        videoSolution: "https://youtu.be/lXVy6YWFcRM",
+        textSolution: "dp: compute max and max-abs-val for each prefix subarr",
+        difficulty: "Medium",
       },
     ],
     []
@@ -35,18 +66,33 @@ const AllAlgos = () => {
   const columns = React.useMemo(
     () => [
       {
-        Header: 'Column 1',
-        accessor: 'col1', // accessor is the "key" in the data
+        Header: 'name',
+        accessor: 'name', // accessor is the "key" in the data
         Filter: FilterAlgosColumn,
       },
       {
-        Header: 'Column 2',
-        accessor: 'col2',
+        Header: 'category',
+        accessor: 'category',
         Filter: FilterAlgosColumn,
       },
       {
-        Header: 'third column',
-        accessor: 'banana',
+        Header: 'problem link',
+        accessor: 'link',
+        Filter: FilterAlgosColumn,
+      },
+      {
+        Header: 'video solution',
+        accessor: 'videoSolution',
+        Filter: FilterAlgosColumn,
+      },
+      {
+        Header: 'text solution',
+        accessor: 'textSolution',
+        Filter: FilterAlgosColumn,
+      },
+      {
+        Header: 'difficulty',
+        accessor: 'difficulty',
         Filter: FilterAlgosColumn,
       },
     ],
