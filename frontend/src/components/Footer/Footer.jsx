69c6fc20c796a0edbbbf5a6b6272cf69b8a51894
kevinledev <kevinle.developer@gmail.com>
2022-04-06 01:48:32 -0700

    create footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,13 +1,70 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./footer.css"
+import "./footer.css";
+import logo from "../../assets/logo-raleway-h40.png"
 
 function Footer() {
-  return(
+  return (
     <div className="footer-container">
-      <h1>Developers</h1>
+      <h1>
+        DEVELOPERS
+        <a
+          href="https://github.com/kevinledev/serene-brook-14726"
+          className="footer-rowname"
+        >
+          Project Repo
+        </a>
+        {/* <div className="footer-rowname">GitHub</div> */}
+      </h1>
+
+      <div className="dev-cards">
+        <div className="dev-card">
+          Kevin Le
+          <div className="devlinks">
+            <a class="fa fa-linkedin"></a>
+            <a
+              href="https://github.com/kevinledev"
+              class="fa-brands fa-github"
+            ></a>
+          </div>
+        </div>
+        <div className="dev-card">
+          Alex Moxley{" "}
+          <div className="devlinks">
+            <a class="fa fa-linkedin"></a>
+            <a
+              href="https://github.com/amoxley-dev"
+              class="fa-brands fa-github"
+            ></a>
+          </div>
+        </div>
+        <div className="dev-card">
+          Elhoussine Elouardy{" "}
+          <div className="devlinks">
+            <a class="fa fa-linkedin"></a>
+            <a
+              href="https://github.com/elhoussine"
+              class="fa-brands fa-github"
+            ></a>
+          </div>
+        </div>
+        <div className="dev-card">
+          Ras Ally{" "}
+          <div className="devlinks">
+            <a class="fa fa-linkedin"></a>
+            <a
+              href="https://github.com/RasAlly"
+              class="fa-brands fa-github"
+            ></a>
+          </div>
+        </div>
+        <Link to={`/`}>
+          {" "}
+          <img src={logo} height="24px"></img>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Footer;
