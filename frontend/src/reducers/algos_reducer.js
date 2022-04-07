1e2163a895cb55f2720210d6a3d20ba5ece18cf6
Ras Ally <rasally434@gmail.com>
2022-04-07 03:29:48 -0400

    Add daily algos

--- frontend/src/reducers/algos_reducer.js
+++ frontend/src/reducers/algos_reducer.js
@@ -1,4 +1,4 @@
-import { RECEIVE_ALL_ALGOS, RECEIVE_ALGO, RECEIVE_USER_ALGO, NEW_USER_ALGO, RECEIVE_ALL_USER_ALGOS } from "../actions/algo_actions";
+import { RECEIVE_ALL_ALGOS, RECEIVE_ALGO } from "../actions/algo_actions";
 
 const AlgosReducer = (state = {}, action) => {
   Object.freeze(state);
@@ -6,20 +6,10 @@ const AlgosReducer = (state = {}, action) => {
 
   switch (action.type) {
     case RECEIVE_ALL_ALGOS:
-      // nextState = action.algos;
       return action.algos;
 
     case RECEIVE_ALGO:
       return action.algo;
-
-    // case RECEIVE_USER_ALGO:
-    //   return action.userAlgo;
-
-    // case RECEIVE_ALL_USER_ALGOS:
-    //   return action.userAlgos;
-
-    // case NEW_USER_ALGO:
-    //   return action.userAlgo;
 
     default:
       return state;
---
+++ frontend/src/reducers/daily_algos_reducer.js
@@ -0,0 +1,44 @@
+import { RECEIVE_TWO_ALGOS } from "../actions/algo_actions";
+
+const dailyAlgosReducer = (state = {}, action) => {
+  Object.freeze(state);
+  const nextState = Object.assign({}, state);
+
+  switch (action.type) {
+
+     case RECEIVE_TWO_ALGOS:
+      //  console.log(Array.isArray(nextState)); 
+      // if (!Array.isArray(nextState)) {
+        const date = new Date();
+        let ms = date.getMilliseconds()
+        console.log(ms);
+
+        let idx = 0;
+        const arr = [];
+        do {
+          idx = Math.floor(Math.random() * 100) + 1;
+          if (idx <= 76 && !arr.includes(idx)) {
+            arr.push(action.algos[idx]) 
+          } 
+        } while (arr.length < 2)
+
+        // setTimeout(() => {
+
+        // })
+
+        return arr
+      // } else {
+      //   return nextState;
+      // }
+        
+    default:
+      return state;
+  }
+}
+// if daily algos is empty
+// return two random algos 
+// make a set timeout for the amount of time until the next day starts
+// when set timeout runs generate two more algos
+// if daily algos is not empty don't do anything
+
+export default dailyAlgosReducer;
