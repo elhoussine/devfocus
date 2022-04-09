8e0811430a193b79ef2bb942b6e12868dd1f6041
kevinledev <kevinle.developer@gmail.com>
2022-04-09 11:24:06 -0700

    styled algos featured

--- frontend/src/components/Algorithms/algos_index.jsx
+++ frontend/src/components/Algorithms/algos_index.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import AllAlgosContainer from "./all_algos_container";
 import ProgressBar from "./progress_bar";
-import './algos_index.css'
+import "./algos_index.css";
 
 class AlgosIndex extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       clock: null,
       algos: [],
-      twoAlgos: []
-    }
+      twoAlgos: [],
+    };
 
     this.setAlgos = this.setAlgos.bind(this);
     // this.randomTwo = this.randomTwo.bind(this);
@@ -21,24 +21,23 @@ class AlgosIndex extends React.Component {
 
   componentDidMount() {
     const date = new Date();
-    this.setState({ clock: date.toLocaleDateString() })
+    this.setState({ clock: date.toLocaleDateString() });
     // console.log(this.props);
-    this.props.fetchAllAlgos()
-    .then(resp => {
+    this.props.fetchAllAlgos().then((resp) => {
       console.log(resp.algos.data);
-      this.setAlgos(resp.algos.data)
-      this.props.receiveTwoAlgos(resp.algos.data)
-    })
+      this.setAlgos(resp.algos.data);
+      this.props.receiveTwoAlgos(resp.algos.data);
+    });
     // this.props.receiveTwoAlgos()
   }
 
   setAlgos(algos) {
-    this.setState({ algos: algos })
+    this.setState({ algos: algos });
   }
 
   getLinkId(algo) {
     let video = algo.videoSolution;
-    const arr = video.split('/'); 
+    const arr = video.split("/");
     return arr[arr.length - 1];
   }
 
@@ -47,43 +46,45 @@ class AlgosIndex extends React.Component {
     const { dailyAlgos } = this.props;
     if (Object.keys(dailyAlgos).length === 0) return null;
     // console.log(dailyAlgos);
-    if (!clock || !algos || !twoAlgos || !dailyAlgos ) return null;
-   return (
-     <div>
-      
-      <div className="feat-alg-cont">
-         <p id="feat-alg">Featured Algorithms</p>
-      </div>
-
-      <div className="show-action-cont">
-
-        <div className="two-daily-algs-cont">
-          {
-            dailyAlgos.map(algo => {
-              // console.log(algo);
-              return (
-                <div className="one-alg-cont">
-                  {/* <a href={algo.link} className="one-alg-link">{algo.link}</a> */}
-                  <iframe className='algo-video'
-                    title='Youtube player'
-                    sandbox='allow-same-origin allow-forms allow-popups allow-scripts allow-presentation'
-                    src={`https://youtube.com/embed/${this.getLinkId(algo)}?autoplay=0`}>
-                  </iframe>
-                  <span className="one-alg-name">{  algo.name }</span>
-                </div>
-              )
-            })
-          }
-          <div id="progress-bar">
-            <span id="completion">completion</span>
-            <ProgressBar userAlgosArr={this.props.userAlgosArr}/>
+    if (!clock || !algos || !twoAlgos || !dailyAlgos) return null;
+    return (
+      <div>
+        <div className="show-action-cont">
+          <div className="two-daily-algs-cont">
+            <div id="progress-bar">
+              <ProgressBar userAlgosArr={this.props.userAlgosArr} />
+              <span id="completion">Your Progress</span>
+            </div>
+            <div className="featured">
+              <div className="feat-alg-cont">
+                <p id="feat-alg">Featured Problems</p>
+              </div>
+              <div className="two-featured">
+                {dailyAlgos.map((algo) => {
+                  // console.log(algo);
+                  return (
+                    <div className="one-alg-cont">
+                      {/* <a href={algo.link} className="one-alg-link">{algo.link}</a> */}
+                      <iframe
+                        className="algo-video"
+                        title="Youtube player"
+                        sandbox="allow-same-origin allow-forms allow-popups allow-scripts allow-presentation"
+                        src={`https://youtube.com/embed/${this.getLinkId(
+                          algo
+                        )}?autoplay=0`}
+                      ></iframe>
+                      {/* <span className="one-alg-name">{algo.name}</span> */}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
           </div>
+          {/* <p>{clock}</p> */}
         </div>
-        {/* <p>{clock}</p> */}
+        <AllAlgosContainer algos={algos} />
       </div>
-       <AllAlgosContainer algos={algos}/>
-     </div>
-   ) 
+    );
   }
 }
 
--- frontend/src/components/Algorithms/progress_bar.jsx
+++ frontend/src/components/Algorithms/progress_bar.jsx
@@ -12,36 +12,36 @@ const ProgressBar = (props) => {
   return (
     <div>
       <div style={{ width: 180, height: 180 }}>
-        <CircularProgressbarWithChildren 
+        <CircularProgressbarWithChildren
           radius={90}
           value={value}
           text={`${value}%`}
           styles={buildStyles({
             // Rotation of path and trail, in number of turns (0-1)
             rotation: 0.5,
-        
+
             // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
-            strokeLinecap: 'round',
-        
+            strokeLinecap: "round",
+
             // Text size
-            textSize: '16px',
-        
+            textSize: "16px",
+
             // How long animation takes to go from one percentage to another, in seconds
             pathTransitionDuration: 0.8,
-        
+
             // Can specify path transition in more detail, or remove it entirely
             // pathTransition: 'none',
-        
+
             // Colors
-            pathColor: `rgba(62, 152, 199)`,//transparency, attach to end of rgba: ${value / 100}
-            textColor: '#f88',
-            trailColor: '#d6d6d6',
-            backgroundColor: '#3e98c7',
+            pathColor: `#e84142`, //transparency, attach to end of rgba: ${value / 100}
+            textColor: "#e84142",
+            trailColor: "#E7E7EF",
+            // backgroundColor: "#e84142",
           })}
         />
       </div>
     </div>
-  )
+  );
 }
 
 export default ProgressBar;
